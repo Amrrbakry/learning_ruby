7d5a901d37932d7706f8d935d3ed2186dd78b14b
Amrr Bakry <amrrbakry17@gmail.com>
2016-01-23 19:46:06 +0200

    added caesar_cipher solution

---
+++ caesar_cipher.rb
@@ -0,0 +1,18 @@
+def caesar_cipher(text, shift_factor)
+	lower_case_letters = ('a'..'z').to_a
+	upper_case_letters = ('A'..'Z').to_a
+	ciphered_text = []
+
+	text.each_char do |letter| 
+		if lower_case_letters.include? letter
+			letter_index = lower_case_letters.index letter
+			letter = lower_case_letters[(letter_index + shift_factor % 26)]
+		elsif upper_case_letters.include? letter
+			letter_index = upper_case_letters.index letter
+			letter = upper_case_letters[(letter_index + shift_factor) % 26]
+		end
+		ciphered_text << letter
+	end
+	puts ciphered_text
+end
+			
