fad88bcb2f6ef4cf8076603e411b6cafa9a217a7
Amrr Bakry <amrrbakry17@gmail.com>
2016-06-27 06:22:15 +0200

    added POST method and its server response

--- web_server/web_browser.rb
+++ web_server/web_browser.rb
@@ -1,11 +1,27 @@
 require 'socket'
+require 'json'
  
 host = 'localhost'     # The web server
 port = 2000                           # Default HTTP port
-path = "/index.html"                 # The file we want 
+get_path = "/index.html"                 # The file we want 
+post_path = "/thanks.html"
 
 # This is the HTTP request we send to fetch a file
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+puts "Do you want to send a GET or a POST request?"
+request = gets.chomp.upcase
+case request
+when "GET"
+	request = "GET #{get_path} HTTP/1.0\r\n\r\n"
+when "POST"
+	print "Please enter a name: "
+	name = gets.chomp.capitalize
+	print "Please enter an email: "
+	email = gets.chomp
+	form_info = { :user => {:name => name, :email => email} }
+	request = "POST #{post_path} HTTP/1.0\r\nContent-Length: #{form_info.to_json.bytesize}\r\n\r\n#{form_info.to_json}"
+else
+	puts "invalid request method"
+end
 
 socket = TCPSocket.open(host,port)  # Connect to server
 
--- web_server/web_server.rb
+++ web_server/web_server.rb
@@ -1,20 +1,34 @@
 require 'socket'
+require 'json'
 
 server = TCPServer.open(2000) # Socket to listen on port 2000
 loop {  # Servers run forever
 	client = server.accept # Wait for a client to connect
-	request = client.gets # getting request from web_browser
-	req_parts = request.split(" ") # parsing the request 
-	request_method, requested_file, http_version = req_parts[0], req_parts[1].split("/")[-1], req_parts[2] #parsing the request
+	request = client.read_nonblock(256) # getting request from web_browser
+	headers, body = request.split("\r\n\r\n", 2) # parsing the request 
+	headers = headers.split(" ")
+	request_method, requested_file, http_version, content_length, = headers[0], headers[1].split("/")[-1], headers[2], headers[4] #parsing the request
 	case request_method
 	when "GET"
 		if File::exist?(requested_file)
+			file = File.open(requested_file).read
 			client.puts "#{http_version} 200 OK"
-			client.puts requested_file.size
-			client.puts File.open(requested_file).read
+			client.puts(Time.now.ctime)
+			client.puts "Content-Type: text/html"
+			client.puts "Content-Length: #{file.bytesize}"
+			client.puts file
 		else
 			client.puts "404 Not Found"
 		end
+	when "POST"
+		file = File.open(requested_file).read
+		client.puts "#{http_version} 200 OK"
+		client.puts(Time.now.ctime)
+		client.puts "Content-Type: text/html"
+		client.puts "Content-Length: #{file.bytesize}"
+		params = JSON.parse(body)
+		user_data = "<li>name: #{params['user']['name']}</li>\n\t  <li>Email: #{params['user']['email']}</li>"
+		client.puts file.gsub("<%= yield %>", user_data)
 	else
 		client.puts "500 Server Error"
 	end	
