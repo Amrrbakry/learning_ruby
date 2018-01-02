bb7e9ca5b4adc793d3c8f3fb3f127496af34e857
Amr El-Bakry <amrrbakry17@gmail.com>
2018-01-02 22:24:00 +0200

    Update linked_list.rb

--- LinkedList/linked_list.rb
+++ LinkedList/linked_list.rb
@@ -1,132 +1,142 @@
-class LinkedList
+module LList
+  class Node
+    attr_accessor :value, :next_node
 
-	class Node
-		attr_accessor :value, :next_node
+    def initialize(value = nil, next_node = nil)
+      @value = value
+      @next_node = next_node
+    end
+  end
 
-		def initialize(value = nil, next_node = nil)
-			@value = value
-			@next_node = next_node
-		end
+  class LinkedList
+    attr_reader :head, :tail, :size
 
-	end
+    def initialize(value)
+      @head = Node.new(value, nil)
+      @tail = @head
+      @size = 1
+    end
 
-	attr_reader :head, :tail
+    def append(value)
+      @tail.next_node = Node.new(value, nil)
+      @tail = @tail.next_node
+      @size += 1
+    end
 
-	def initialize(value)
-		@head = Node.new(value, nil)
-		@tail = @head
-		@@size = 1
-	end
+    def prepend(value)
+      @head = Node.new(value, @head)
+      @size += 1
+    end
 
-	# adds a new node to the end of the list - increments the size of the list
-	def append(value)
-		@tail.next_node = Node.new(value, nil)
-		@tail = @tail.next_node
-		@@size += 1
-	end
+    def at(index)
+      return @head if index == 1
 
-	# adds a new node to the start of the list - increments the size of the list
-	def prepend(value)
-		@head = Node.new(value, @head)
-		@@size += 1
-	end
+      return @tail if index == @size
 
-	# return the number of nodes in the list
-	def size
-		@@size
-	end
+      return nil if index > @size || index.zero?
 
-	# returns the node at the given index
-	def at(index)
-		return @head if index == 1
-		return @tail if index == @@size
-		return nil if index > @@size
-		node = @head
-    pos = 1
-    while node.next_node != nil
-      pos += 1
-      node = node.next_node
-      return node if index == pos
-    end 
-	end
+      current_node = @head
+      1.upto(@size) do |i|
+        return current_node if i == index
 
-	# deletes the last node in the list
-	def pop
-		return "Cannot delete the head of the list!" if @@size == 1
-		node_before_last = at( (@@size - 1) )
-		node_before_last.next_node = nil
-		@tail = node_before_last
-		@@size -= 1
-	end
+        current_node = current_node.next_node
+        next
+      end
+    end
 
-	# returns true if the list contains the given value
-	def contains?(value)
-		pos = 1
-		node = @head
-		while pos != (@@size + 1)
-			if node.value == value
-				return true
-			else
-				node = node.next_node
-				pos += 1
-				next
-			end
-		end
-		false
-	end
+    def pop
+      return 'Cannot delete the head of the list' if @size == 1
 
-	# returns the index of the node that has the given value or nil if not found
-	def find(value)
-		if contains?(value)
-			pos = 1
-			node = @head
-			while pos != (@@size + 1)
-				if node.value == value
-					return pos
-				else
-					node = node.next_node
-					pos += 1
-					next
-				end
-			end
-		else
-			nil
-		end
-	end
+      node_before_last = at((@size - 1))
+      node_before_last.next_node = nil
+      @tail = node_before_last
+      @size -= 1
+    end
 
-	# prints the list objects in a string format
-	def to_s
-		node = @head
-		@@size.times do |i|
-			print "( #{node.value} ) -> " 
-			node = node.next_node
-			if i == ( @@size - 1 )
-				puts "nil"
-			end
-		end
-	end
+    def contains?(value)
+      current_node = @head
+      1.upto(@size) do
+        return true if value == current_node.value
 
-	# inserts value at the given index
-	def insert_at(index, value)
-	prev_node = at(index - 1)
-	current = at(index)
-	current = Node.new(value, current)
-	prev_node.next_node = current
-	@@size += 1
-	end
+        current_node = current_node.next_node
+        next
+      end
+      false
+    end
 
-	# removes the node at the given index
-	def remove_at(index)
-	return nil if index > @@size
-		case index
-		when 1 then @head = at(2); @@size -= 1
-		when @@size then pop
-		else
-			prev_node = at(index - 1)
-			n_node = at(index + 1)
-			prev_node.next_node = n_node
-			@@size -= 1
-		end
-	end
+    def find(data)
+      current_node = @head
+      1.upto(@size) do |i|
+        return i if current_node.value == data
+
+        current_node = current_node.next_node
+        next
+      end
+      nil
+    end
+
+    def to_s
+      current_node = @head
+      1.upto((@size + 1)) do |i|
+        return puts 'nil' if i > @size || current_node.nil?
+
+        print "( #{current_node.value} ) -> "
+        current_node = current_node.next_node
+      end
+    end
+
+    def insert_at(index, value)
+      node_before_index = at((index - 1))
+      node_at_index = at(index)
+      new_node = Node.new(value, node_at_index)
+      @head = new_node if index == 1
+      @tail = new_node if index == @size + 1
+      node_before_index.next_node = new_node if node_before_index
+      @size += 1
+    end
+
+    def remove_at(index)
+      return nil if index > @size
+
+      if index == @size
+        pop
+        @size -= 1
+        return
+      end
+
+      if index == 1
+        @head = at(2)
+        @size -= 1
+        return
+      end
+
+      node_before_index = at((index - 1))
+      node_after_index = at((index + 1))
+      node_at_index = at(index)
+      node_at_index.value = nil
+      node_at_index.next_node = nil
+      node_before_index.next_node = node_after_index if node_after_index
+      @size -= 1
+    end
+  end
 end
 
+linked_list = LList::LinkedList.new('first')
+linked_list.append('second')
+linked_list.append('third')
+linked_list.append('fourth')
+linked_list.to_s
+puts linked_list.at(4).value # fourth
+p linked_list.at(7) # nil
+linked_list.pop
+linked_list.to_s
+puts linked_list.contains?('seventh') # false
+puts linked_list.contains?('second') # true
+linked_list.insert_at(1, 'inserted_first')
+linked_list.to_s
+linked_list.remove_at(3)
+linked_list.to_s
+linked_list.prepend('prepended_first')
+linked_list.to_s
+linked_list.insert_at(5, 'inserted_last')
+linked_list.to_s
